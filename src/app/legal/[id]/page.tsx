2a7350c282253bdcfcca17c36bd7e3dd44431029
toby7002 <144540995+toby7002@users.noreply.github.com>
2023-12-25 19:14:40 +0700

    Update legal pages

--- src/app/legal/[id]/page.tsx
+++ src/app/legal/[id]/page.tsx
@@ -2,7 +2,7 @@ export default function ({ params }: { params: { id: string } }) {
 	const id = params.id;
 
 	return (
-		<div className="w-11/12 mx-auto">
+		<div className="w-11/12 mx-auto my-6">
 			<div className="grid gap-5 lg:grid-rows-1 lg:grid-cols-3 grid-cols-1 grid-rows-2 place-items-center">
 				<ul className="menu w-full rounded-box lg:col-span-1 bg-base-300">
 					<li className="menu-title text-lg text-base-content">
